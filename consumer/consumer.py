b4ac60ce077d293a5b3c97e2138e464155ba7131
oussama <nadranisama@gmail.com>
2022-01-16 17:01:16 +0100

    affichage producer change

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,31 +2,28 @@ from kafka import KafkaConsumer
 from kafka.consumer.fetcher import ConsumerRecord
 import json
 
-topicName = 'testTopic'
+topicName = 'NewTopic'
 structurData={"number":0,
-"contract_name":"",
-"name":"",
-"address":"",
-"position":{"lat":0,"lng":0},
-"banking":False,
-"bonus":False,
-"bike_stands":0,
-"available_bike_stands":0,
-"available_bikes":0,
-"status":"OPEN",
-"last_update":1641522522000}
-
+	"contract_name":"",
+	"name":"",
+	"address":"",
+	"position":{"lat":0,"lng":0},
+	"banking":False,
+	"bonus":False,
+	"bike_stands":0,
+	"available_bike_stands":0,
+	"available_bikes":0,
+	"status":"OPEN",
+	"last_update":1641522522000}
 listData=[structurData]
 def getIndexMessage(value):
 	for i in range(len(listData)-1):
-		# print("list",type(listData[i]))#,listData[i]['number']
 		if listData[i]['number']==value['number']:
 			return i
 	return -1
 
 consumer = KafkaConsumer(topicName, bootstrap_servers=['localhost:9092'],
-value_deserializer=lambda m: json.loads(m.decode('utf-8')))#,auto_offset_reset='earliest')# , group_id='my-group'
-
+value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 for message in consumer:
 	# indique si un element a etait modifier ou ajouter dans la list
@@ -49,5 +46,3 @@ for message in consumer:
 		"\t available_bike_stands",value['available_bike_stands'],
 		"\tavailable_bikes",value['available_bikes'],
 		"\tname",value['name'])
-
-
--- producer/producer.py
+++ producer/producer.py
@@ -6,7 +6,6 @@ import time
 # GET Station information as JSON from api 
 url = "https://api.jcdecaux.com/vls/v1/stations?contract=marseille&apiKey=d5d56f9105fbbdfd8c1e53072eb3b2fba2bd41c4"
 
-
 def on_send_success(record_metadata):
 		print(record_metadata.topic)
 		print(record_metadata.partition)
@@ -14,16 +13,18 @@ def on_send_success(record_metadata):
 
 def on_send_error(excp):
 		log.error('I am an errback', exc_info=excp)
-
 # Initialise Kafka Producer 
-topicName='testTopic'
+topicName='NewTopic'
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
 	value_serializer=lambda m: json.dumps(m).encode('utf-8'))
 # Send Json data to Consumer 
+print("Start Producer using bootstrap server: localhost:9092 in topic NewTopic")
 while True:
 	r = requests.get(url)
 	print(r.headers['content-type']) # print json file 
 	data = r.json()
 	for station in data:
 		producer.send(topicName, station).add_callback(on_send_success).add_errback(on_send_error)
-	time.sleep(86400) # continue once a day 
+	# continue once a day 86400s == 1 day
+	time.sleep(86400) 
+
